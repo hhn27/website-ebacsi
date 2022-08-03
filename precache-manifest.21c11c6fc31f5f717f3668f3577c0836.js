109afab19c738690beb67a09a0fb75acf86876a0
hhn27 <hoang7323@gmail.com>
2022-08-03 14:20:59 +0700

    Updates

---
+++ precache-manifest.21c11c6fc31f5f717f3668f3577c0836.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e1dfbccc80954eb3705",
+    "url": "/website-ebacsi/static/css/main.fc2d1065.chunk.css"
+  },
+  {
+    "revision": "8e1dfbccc80954eb3705",
+    "url": "/website-ebacsi/static/js/main.8e1dfbcc.chunk.js"
+  },
+  {
+    "revision": "1497e999a888b9fa61ca",
+    "url": "/website-ebacsi/static/js/1.1497e999.chunk.js"
+  },
+  {
+    "revision": "b53fc4a8077cb3f57c88",
+    "url": "/website-ebacsi/static/js/2.b53fc4a8.chunk.js"
+  },
+  {
+    "revision": "fa90d74f5133ad2bf555",
+    "url": "/website-ebacsi/static/js/runtime~main.fa90d74f.js"
+  },
+  {
+    "revision": "84aa4315338d8e40a04de6360c9eb900",
+    "url": "/website-ebacsi/static/media/greyimage.84aa4315.jpg"
+  },
+  {
+    "revision": "8a40c2922bb9d866603794a994489493",
+    "url": "/website-ebacsi/static/media/Banner 2.8a40c292.jpg"
+  },
+  {
+    "revision": "d89b9a68dcee813860e95417f2f8e5f4",
+    "url": "/website-ebacsi/static/media/Banner 1 (1920x640).d89b9a68.png"
+  },
+  {
+    "revision": "e622d03d6e2d8838da9bee72feb46f2c",
+    "url": "/website-ebacsi/static/media/Banner 2 Tong dai.e622d03d.jpg"
+  },
+  {
+    "revision": "323295a66ecfe1104ef13a53258d91ab",
+    "url": "/website-ebacsi/static/media/Iphone.323295a6.png"
+  },
+  {
+    "revision": "7eb3caa9e6009d2f4c299a0d12bac36a",
+    "url": "/website-ebacsi/static/media/Website---eBacsi---final---cut_47.7eb3caa9.jpg"
+  },
+  {
+    "revision": "4efdfe7fad25d67f592c17737188d398",
+    "url": "/website-ebacsi/static/media/Website---eBacsi---final---cut_49.4efdfe7f.jpg"
+  },
+  {
+    "revision": "a15f8b7d57a6a2ac6a44ecb3bd695caf",
+    "url": "/website-ebacsi/static/media/Website---eBacsi---final---cut_54.a15f8b7d.jpg"
+  },
+  {
+    "revision": "c0313e124529425ac45087d2ec92f177",
+    "url": "/website-ebacsi/static/media/Website---eBacsi---final---cut_55.c0313e12.jpg"
+  },
+  {
+    "revision": "096ab0bd72163ca1a48c2fe9eb1880fd",
+    "url": "/website-ebacsi/static/media/Website---eBacsi---final---cut_56.096ab0bd.jpg"
+  },
+  {
+    "revision": "215314570516797adceb6ad318de7164",
+    "url": "/website-ebacsi/static/media/Banner 3.21531457.jpg"
+  },
+  {
+    "revision": "a582c9937ca5c2b4767e63489e5efd60",
+    "url": "/website-ebacsi/static/media/BV BUU DIEN TPHCM.a582c993.png"
+  },
+  {
+    "revision": "be78e24cce9b253cdce4060ff9f62290",
+    "url": "/website-ebacsi/static/media/DR.BINH.be78e24c.png"
+  },
+  {
+    "revision": "b24711065925b8fcdbdbed7c31e6b32f",
+    "url": "/website-ebacsi/static/media/Banner 4.b2471106.jpg"
+  },
+  {
+    "revision": "03b11bdca193827498a99d19fad1781b",
+    "url": "/website-ebacsi/static/media/Anh nam 28.03b11bdc.jpg"
+  },
+  {
+    "revision": "a45e303d1af2776af5a92074405a98f3",
+    "url": "/website-ebacsi/static/media/Bac Hai nghi huu.a45e303d.jpg"
+  },
+  {
+    "revision": "b361e684676d0c310bf58ad11469bdc1",
+    "url": "/website-ebacsi/static/media/chi Huong 30 noi tro.b361e684.jpg"
+  },
+  {
+    "revision": "2b1476ecced70dcf01de3c3fa3e93916",
+    "url": "/website-ebacsi/static/media/Website---eBacsi---final---cut_14.2b1476ec.png"
+  },
+  {
+    "revision": "2c3ec20e58d18785b7ebbe994679b924",
+    "url": "/website-ebacsi/index.html"
+  }
+];
